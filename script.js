a902e030fd82a818ccaf353067574790044e9c65
Ray Zin Ng <s10222457@connect.np.edu.sg>
2022-11-19 23:17:32 +0800

    updated week5 diary

--- script.js
+++ script.js
@@ -13,12 +13,17 @@ const testimonials = [
     {
       week: "3",
       testimonial:
-        "The third week was the start of coding the application. I have a strong feeling that I need to hard carry (anticipated since week1) as my team has 2 weak coders and only left me and chunkai. Chun kai also seems to retaliate by learning new things, in this case coding in a HTML website. We have been through this in ID, so I am not really sure why. We also visited the dev ops center in NP. It was pretty cool as I have never seen anything like this in real life before. The room also has a funky smell… ",
+        "The third week was the start of coding the application. I have a strong feeling that I need to hard carry (anticipated since week1) as my team has 2 weak coders and only left me and chunkai. Chun kai was also not confident with HTML, CSS, javascript. We also visited the dev ops center in NP. It was pretty cool as I have never seen anything like this in real life before. The room also has a funky smell… ",
     },
     {
       week: "4",
       testimonial:
         "The fourth week I am starting to feel very stressed.  The technical part is doing well, however, I began to further doubt the idea and needed to start all over into ideation.  The problem is that the idea has a flaw where the donors do get any gains from donating which would promote donating. Hence, I gather the team to start ideating about that problem. As more ideas get poured in by my teammates and teacher, I get extremely confused and lost as I do not know what is best to do. I asked myself whether it is feasible and realistic. I am still very confused and do not know how to continue. To make it worse, next week is the submission dateline alongside with a lot of work from whitespace week and week 4. It is going to hell week and I am not sure if my team is able to pull through with everything in place. Right now, I will just stick to finishing up the basic frame of the website and continue to prototype with the idea given to us. ",
+    },
+    {
+      week: "5",
+      testimonial:
+        "This was the final stretch, the hackathon was the finale of assignment 1 and also no more PFD lessons for the remaining term. I was excited and stressed at the same time, wanting it to all end so that I can focus on other work in the future. I have worked tirelessly since week 2 to code the application from start to finish based on what I had planned. Although not all of the functions were implemented, it was still as good as finished. My teammates also helped do other side hustles like slides, pitching, marketing for the website e.g email and instagram account and error handling plus coding. Everyone came together to give the final push. It turned out to be a huge success as the pitching/demo went well as well as winning the best project for our challenge topic “what does sustainability look like in Ngee Ann Polytechnic?”. We also got prizes for getting first. I received a razer mouse and a toblerone. It was also my first hackathon and I am really proud of the outcome. ",
     },
   ];
   
